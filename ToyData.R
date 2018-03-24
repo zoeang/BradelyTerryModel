8967ff8a75bc414a941b5325c13f98c14fd1c303
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-24 15:41:01 -0500

    Create more useful toy data
    This will help when calculating lambda. I think

--- ToyData.R
+++ ToyData.R
@@ -1,5 +1,10 @@
-toydata<-data.frame(rep(1,30), sample(1:100, 30, replace=T), sample(c(0,1), 30, replace=T))
+#CREATE FAKE DATA
+toydata<-data.frame(sort(rep(seq(1,10),10)), sample(1:10, 100, replace=T), sample(c(0,1), 100, replace=T))
 colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
 toydata
 table(toydata$DocIDj)
 
+#Remove rows where doci=docj
+toydata$delete<- (toydata2$DocIDi==toydata2$DocIDj)
+toydata2<-subset(toydata,delete=="FALSE")
+toydata2

0e06406098560a573edbb8e245834a6bb1bcfd5e
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-27 13:28:26 -0500

    added test files

--- devtools.bradleyterry/developmentfile.R
+++ devtools.bradleyterry/developmentfile.R
@@ -5,6 +5,7 @@ setwd("/Users/benjaminschneider/Documents/GitHub/BradelyTerryModel/devtools.brad
 current.code<-as.package("bradleyterry")
 load_all(current.code)
 document(current.code)
+test(current.code)
 
 
 toydata<-data.frame(rep(1,30), sample(1:100, 30, replace=T), sample(c(0,1), 30, replace=T))

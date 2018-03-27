482a2a0bb0b9a50c24dfd42d4502af5a62b9ed14
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-03-27 13:27:10 -0500

    update the dummy data
    dummy dataset and lambda info updated

--- devtools.bradleyterry/developmentfile.R
+++ devtools.bradleyterry/developmentfile.R
@@ -7,19 +7,32 @@ load_all(current.code)
 document(current.code)
 
 
-toydata<-data.frame(rep(1,30), sample(1:100, 30, replace=T), sample(c(0,1), 30, replace=T))
-colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
-lambdai<-runif(30)
-lambdaj<-runif(30)
-
-
-lambdai<-as.vector(c(.25,.25))
-lambdaj<-as.vector(c(.25,.25))
-
-toydata<-data.frame(rep(1,2), sample(1:100, 2, replace=T), sample(c(0,1), 2, replace=T))
+#CREATE FAKE DATA
+toydata<-data.frame(sort(rep(seq(1,10),10)), sample(1:10, 100, replace=T), sample(c(0,1), 100, replace=T))
 colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
 toydata
-dataset<-toydata
+table(toydata$DocIDj)
+
+#Remove rows where doci=docj
+toydata$delete<- (toydata$DocIDi==toydata$DocIDj)
+toydata2<-subset(toydata,delete=="FALSE")
+toydata2<-toydata2[,-4]
+toydata2
+
+#create lambda data frame
+lambda<-data.frame(c(1:10),runif(10))
+lambda
 
 bradleyterry(1,1,lambdai,lambdaj,toydata)
 
+bradleyterry<-function(a,b,i,lambda,dataset){
+  sumvec<-NULL
+  
+  for (i in 1:length(lambdai)){
+    sumunit<-(1/(lambdai[i]+lambdaj[i]))
+    sumvec<-as.vector(c(sumvec,sumunit))
+  }
+  summationterm<-sum(sumvec)
+  output<-(a-1+sum(dataset$Choose))/(b+summationterm)
+  return(output)
+}

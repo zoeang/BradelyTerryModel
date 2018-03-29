fe56ccc93c2c1d8f63303b30407fa37ebd33df87
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-29 10:28:21 -0500

    Made consistent toydata
    It will always be the same so we can actually write a test!

--- devtools.bradleyterry/bradleyterry/tests/testthat/test_onedocID.R
+++ devtools.bradleyterry/bradleyterry/tests/testthat/test_onedocID.R
@@ -1,5 +1,22 @@
-context("Name of Context")
+context("Correct estimation of Lambda for one Doc ID")
+#CREATE FAKE DATA
+toydata<-data.frame(sort(rep(seq(1,10),10)), sample(1:10, 100, replace=T), sample(c(0,1), 100, replace=T))
+colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
+toydata
+table(toydata$DocIDj)
 
-test_that("Label of group of tests",{
-  expect_that()
+#Remove rows where doci=docj
+toydata$delete<- (toydata$DocIDi==toydata$DocIDj)
+toydata2<-subset(toydata,delete=="FALSE")
+toydata2<-toydata2[,-4]
+dataset<-toydata2
+#create lambda data frame
+lambda<-data.frame(c(10:1),.5)
+colnames(lambda)<-c('DocId', 'Lambda')
+
+
+test_that("Scalar lambda output",{
+  expect_that(bradleyterry(1,1,10,lambda,dataset), equals(4/11))
 })
+lambda<-data.frame(c(1:10),runif(10))
+colnames(lambda)<-c('DocId', 'Lambda')
--- devtools.bradleyterry/developmentfile.R
+++ devtools.bradleyterry/developmentfile.R
@@ -8,18 +8,26 @@ load_all(current.code)
 document(current.code)
 test(current.code)
 
-
+docjdat<-c(2,seq(2,10,1), 1, seq(3,10,1), 1,2, seq(4,10,1),1, seq(1,3,1), seq(5,10,1),
+          1, seq(1,4,1), seq(6,10,1),
+           1,3,seq(1,5,1), seq(7,10,1),
+           1, 3,seq(1,6,1), seq(8,10,1),
+           1, 3,seq(1,7,1), seq(9,10,1),
+          1, 3, seq(1,9,1),1, seq(2,8,1))
+rep(c(1,0,1,1,0),20)
 #CREATE FAKE DATA
-toydata<-data.frame(sort(rep(seq(1,10),10)), sample(1:10, 100, replace=T), sample(c(0,1), 100, replace=T))
+dataset<-toydata<-data.frame(sort(rep(seq(1,10),10)), docjdat, rep(c(1,0,1,1,0),20))
 colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
 toydata
 table(toydata$DocIDj)
 
 #Remove rows where doci=docj
-toydata$delete<- (toydata$DocIDi==toydata$DocIDj)
-toydata2<-subset(toydata,delete=="FALSE")
-toydata2<-toydata2[,-4]
-dataset<-toydata2
+#this is not irrelevant
+#toydata$delete<- (toydata$DocIDi==toydata$DocIDj)
+#toydata2<-subset(toydata,delete=="FALSE")
+#toydata2<-toydata2[,-4]
+#dataset<-toydata2
+
 
 #create lambda data frame
 lambda<-data.frame(c(10:1),runif(10))

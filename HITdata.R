9cb632d2e4f9e9428bd5e8284ac5c8e5139dba6b
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-01 13:56:48 -0500

    Create a lambda
    Used exampleHITs data

--- HITdata.R
+++ HITdata.R
@@ -1,5 +1,11 @@
 HIT<-read.csv("C:/Users/zoeja/OneDrive/Documents/Spring2018/R/BradelyTerryModel/exampleHITs.csv", header=T)
-HIT
-lambdaDF<-data.frame(HIT$document_id[1:100],round(runif(100), 3)) 
-colnames(lambdaDF)<-(c("DocId", "Lambda"))
-lambdaDF
+colnames(HIT)<-c("DocIDi", "DocIDj", "Choose")
+#Create a lambda dataframe where each DocIDi has a lambda; unique DocIDj will have a lambda value; there should be four columns
+lambdaDF<-data.frame(HIT$DocIDi[1:100],HIT$DocIDj[1:100]) 
+uniqueDocID<-c(unique(lambdaDF[,2]),unique(lambdaDF[,1]))
+set.seed(13)
+lambdaDocIDj<-round(runif(uniqueDocID),3)
+lambda<-data.frame(uniqueDocID, lambdaDocIDj)
+
+#lambda dataframe
+lambda

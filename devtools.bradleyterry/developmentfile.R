bb3106300d70a97ef85ebc6805b49c01bbc88231
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-01 12:56:27 -0500

    DevFile

--- devtools.bradleyterry/developmentfile.R
+++ devtools.bradleyterry/developmentfile.R
@@ -46,20 +46,21 @@ obj1
 ddply(.data=dataset, .variables=c("DocIDi", "DocIDj"), .fun=bradleyterry(a,b,id,lambda,dataset))
 #end example===================================================================
 
+#### FUNCTION 1 #######
 bradleyterry<-function(a,b,id,lambda,dataset){
-  subsetdata<-dataset[dataset$DocIDi %in% id,]
-  newlambda<-lambda[lambda$DocId %in% id,]
-  sumvec<-NULL
-  lambdavec<-NULL
+  subsetdata<-dataset[dataset$DocIDi %in% id,]#this subsets the dataset down to just the observations with the id that we are looking at
+  newlambda<-lambda[lambda$DocId %in% id,]#this extratcs the specific lambda amount we want to upgrade for the purpose of the equation 
+  sumvec<-NULL #create null vectors to store our sum elements
+  lambdavec<-NULL #create null vector to extract the lambda elements we want
   for(i in subsetdata$DocIDj){
-    lambdavec<-c(lambdavec,lambda$Lambda[i])
+    lambdavec<-c(lambdavec,lambda$Lambda[i]) #This extracts all of the lambdaj values that we will work with in the equation below
   }
   for (i in 1:nrow(subsetdata)){
-    sumunit<-(1/(newlambda$Lambda+lambdavec[i]))
-    sumvec<-as.vector(c(sumvec,sumunit))
+    sumunit<-(1/(newlambda$Lambda+lambdavec[i])) #This creates the summation term unit by unit with the lambda i value and all of the respective lambda js 
+    sumvec<-as.vector(c(sumvec,sumunit)) #this makes a vector of the summation terms
   }
-  summationterm<-sum(sumvec)
-  output<-(a-1+sum(subsetdata$Choose))/(b+summationterm)
+  summationterm<-sum(sumvec) #here we sum the terms of the vector to plug into the equation
+  output<-(a-1+sum(subsetdata$Choose))/(b+summationterm) #this is where we finish up the equation and plug in all of our respective parts
   return(output)
 }
 
@@ -69,38 +70,39 @@ lambda<-data.frame(c(10:1),.5)
 colnames(lambda)<-c('DocId', 'Lambda')
 
 bradleyterry(1,1,1,lambda,dataset)
+<<<<<<< HEAD
 #==============================================
 bradleyterry<-function(a,b,id,lambda,dataset){
+=======
+bradleyterry.multid(1,1,id,lambda,dataset)
+
+#### FUNCTION 2 #######
+bradleyterry.multid<-function(a,b,id,lambda,dataset){
+>>>>>>> afa0a3b89f5c043b21c40de934f5938e7dbd968e
   updatedlambda<-NULL
   for (i in id){
-    subsetdata<-dataset[dataset$DocIDi %in% i,]
-    newlambda<-lambda[lambda$DocId %in% i,]
-    sumvec<-NULL
-    lambdavec<-NULL
-    for(i in subsetdata$DocIDj){
-      lambdavec<-c(lambdavec,lambda$Lambda[i])
+    newlambda<-bradleyterry(a,b,i,lambda,dataset)
+    updatedlambda<-c(updatedlambda,newlambda)
     }
-    for (i in 1:nrow(subsetdata)){
-      sumunit<-(1/(newlambda$Lambda+lambdavec[i]))
-      sumvec<-as.vector(c(sumvec,sumunit))
-    }
-    summationterm<-sum(sumvec)
-    output<-(a-1+sum(subsetdata$Choose))/(b+summationterm)
-    updatedlambda<-c(updatedlambda,output)}
   output<-cbind(id,updatedlambda)
   output<-as.data.frame(output)
   colnames(output)<-c('DocId','Lambda')
   return(output)
 }
 
+
+#### FUNCTION 3 #######
 iterative.bt<-function(a,b,id,lambda,dataset, iterations){
   for (i in 1:iterations){
-    lambda<-bradleyterry(a,b,id,lambda,dataset)}
+    lambda<-bradleyterry.multid(a,b,id,lambda,dataset)}
   return(lambda)
 }
 
+bradleyterry(1,1,1,lambda,dataset)
+bradleyterry.multid(1,1,id,lambda,dataset)
+iterative.bt(1,1,id,lambda,dataset,1)
 
 newlambda<-bradleyterry(1,1,id,lambda,dataset)
 newlambda1<-bradleyterry(1,1,id,newlambda,dataset)
-bradleyterry(1,1,id,newlambda1,dataset)
-iterative.bt(1,1,id,lambda,dataset,50)
+bradleyterry(1,1,1,newlambda1,dataset)
+iterative.bt(1,1,id,lambda,dataset,1)

92ddf2c134e265059fd0fd73992e94d9b82ffa21
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-02 09:10:10 -0500

    Clean up devFile

--- devtools.bradleyterry/developmentfile.R
+++ devtools.bradleyterry/developmentfile.R
@@ -33,19 +33,6 @@ table(toydata$DocIDj)
 lambda<-data.frame(c(10:1),runif(10))
 colnames(lambda)<-c('DocId', 'Lambda')
 
-library(plyr)
-ddply(toydata2,2)
-#ply example from Lecture 8====================================================
-dd<-data.frame(matrix(rnorm(216),72,3),c(rep("A",24),rep("B",24),rep("C",24)),c(rep("J",36),rep("K",36)))
-colnames(dd) <- c("v1", "v2", "v3", "dim1", "dim2")
-head(dd)
-dd
-obj1 <- ddply(.data=dd, .variables=c("dim1","dim2"), .fun=function(df) mean(df$v1))
-obj1
-#maybe?
-ddply(.data=dataset, .variables=c("DocIDi", "DocIDj"), .fun=bradleyterry(a,b,id,lambda,dataset))
-#end example===================================================================
-
 #### FUNCTION 1 #######
 bradleyterry<-function(a,b,id,lambda,dataset){
   subsetdata<-dataset[dataset$DocIDi %in% id,]#this subsets the dataset down to just the observations with the id that we are looking at

27ab32b9ffa56175702adddcc69a6d3c086a3dfc
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-10 13:20:06 -0500

    Apply function

--- applyExample.R
+++ applyExample.R
@@ -8,8 +8,17 @@ dd
 obj1 <- ddply(.data=dd, .variables=c("dim1","dim2"), .fun=function(df) mean(df$v1))
 obj1
 #maybe?
-ddply(.data=dataset, .variables=c("id"), .fun=bradleyterry(1,0,id=1,lambda,dataset))
+ddply(.data=dataset, .variables=c("id"), .fun=bradleyterry(1,0,id=3,lambda,dataset))
 #end example===================================================================
 
 #will apply work? The .variables argument is the column of the dataframe to iterate over,
 #but we want to interate over an argument of the function(id), not a dataset column
+head(dataset)
+sapply(unique(dataset$DocIDi), function(x) bradleyterry(1,0,id=x, lambda, dataset))
+bradleyterry.multid.apply<-function(a, b, id, lambda, dataset){
+  sapply(id, function(x) bradleyterry(a,b,id=x, lambda, dataset))
+}
+
+sapply(unique(dataset$DocIDi), function(x) paste(x, bradleyterry(1,0,id=x, lambda, dataset)))
+
+

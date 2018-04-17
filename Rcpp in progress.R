b8193c97085d3eb637d0af09bf14394fe7817b33
HungryLim <36051245+HungryLim@users.noreply.github.com>
2018-04-17 14:13:39 -0500

    y
    e

---
+++ Rcpp in progress.R
@@ -0,0 +1,38 @@
+library(Rcpp)
+library("RcppArmadillo")
+/*** subset a matrix with (), dataframe []?
+cppFunction(
+  data.gen<-function(NumericVector lambda,NumericVector n)
+  ouput.lambda<- R_Nilvalue
+  for(int i=0; i<n; i++){
+    lams<-csample_num(lambda[1], int 2)
+    lambdavec<-R_Nilvalue
+    for (int i=0; i<n;i++){
+      labmbdavec<-c(labmbdavec,lambda[2][i])
+    }
+    prob<-lambdavec[1]/(lambdavec[1]+lambdavec[2])
+    choose<-csample_num(c(0,1), int 1, bool replace,NumericVector prob = NumericVector::create())
+  
+    })
+)
+
+
+cppFunction(
+  data.gen<-function(NumericVector lambda,NumericVector b,NumericVector id,NumericVector lambda,dataset )
+  subsetdata<-std::fin  
+    
+    
+    
+    
+    ouput.lambda<- R_Nilvalue
+  for(int i=0; i<n; i++){
+    lams<-csample_num(lambda[1], int 2)
+    lambdavec<-R_Nilvalue
+    for (int i=0; i<n;i++){
+      labmbdavec<-c(labmbdavec,lambda[2][i])
+    }
+    prob<-lambdavec[1]/(lambdavec[1]+lambdavec[2])
+    choose<-csample_num(c(0,1), int 1, bool replace,NumericVector prob = NumericVector::create())
+    
+  })
+)

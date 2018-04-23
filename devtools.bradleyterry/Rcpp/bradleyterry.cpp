129421749bb1b534e792ae256dadce24f386fd1b
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-23 17:55:29 -0500

    syntax change

--- devtools.bradleyterry/Rcpp/bradleyterry.cpp
+++ devtools.bradleyterry/Rcpp/bradleyterry.cpp
@@ -7,7 +7,7 @@ double bradleyterryeasy(int a, int b, int id, List lambda, List datset ){
   NumericVector sumvec; // create empty vectors to store our sum elements
   NumericVector lambdavec; //create empty vector to extract the lambda elements we want
 
-  for(int i=0; i<datset[[id]].nrow; ++i){ //i needs to be length of test1[[4990]][,2]
+  for(int i=0; i<datset[[id]].nrow(); ++i){ //i needs to be length of test1[[4990]][,2]
     //NumericVector lambdaj=datset[[id]][,2] //vector of DocIDj for corresponding DocIDi comparison
     double sumunit=(1/(lambda[[id]][,2]+lambda[[datset[[id]][,2][i]]][,2]));// 1/ lambdaI+ lambdaJ
     NumericVector sumvec= c(sumvec, sumunit); //vector of 1/lambdaI+lambdaJ for each comparison of DocIDI to DocIDJ

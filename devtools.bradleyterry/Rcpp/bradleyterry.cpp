c93a4e925989ad52f841e77c6682873ccdf56b28
zoeang <36051346+zoeang@users.noreply.github.com>
2018-04-24 13:38:03 -0500

    Rewrite Func 1 for subsetting; still needs correction
    DataFrame get_list_elem(List x, int idx) {
      DataFrame tmp = as<DataFrame>(x[idx]);
      return tmp;
    }

--- devtools.bradleyterry/Rcpp/bradleyterry.cpp
+++ devtools.bradleyterry/Rcpp/bradleyterry.cpp
@@ -1,19 +1,24 @@
 #include<Rcpp.h>
 
 using namespace Rcpp;
-
+DataFrame get_list_elem(List x, int idx) {
+  DataFrame tmp = as<DataFrame>(x[idx]);
+  return tmp;
+}
 // [[Rcpp::export]]
 double bradleyterryeasy(int a, int b, int id, List lambda, List datset ){
   NumericVector sumvec; // create empty vectors to store our sum elements
   NumericVector lambdavec; //create empty vector to extract the lambda elements we want
-
-  for(int i=0; i<datset[[id]].nrow(); ++i){ //i needs to be length of test1[[4990]][,2]
+  DataFrame datsetID=as<DataFrame>(datset[id]); //create dataframe for subsetting in loop 
+  DataFrame lambdaID=as<DataFrame>(lambda[id]); //create dataframe for subsetting in loop 
+  NumericVector datDocj= datsetID[1];//vector of DocIDj corresponding to DocIDi designated by id
+  for(int i=0; i<datsetID.nrow(); ++i){ //i needs to be length of test1[[4990]][,2]; solved?
     //NumericVector lambdaj=datset[[id]][,2] //vector of DocIDj for corresponding DocIDi comparison
-    double sumunit=(1/(lambda[[id]][,2]+lambda[[datset[[id]][,2][i]]][,2]));// 1/ lambdaI+ lambdaJ
+    double sumunit=(1/(lambdaID[1]+lambda[datDocj[i]][1]));// 1/ lambdaI+ lambdaJ
     NumericVector sumvec= c(sumvec, sumunit); //vector of 1/lambdaI+lambdaJ for each comparison of DocIDI to DocIDJ
-    };
-  NumericVector summationterm = sum(sumvec) ;
-  NumericVector output = (a-1+sum(datset[[id]][,3])/(b+summationterm);
+    }
+  NumericVector summationterm = sum(sumvec);
+  NumericVector output = (a-1+sum(datsetID[2])/(b+summationterm);
   return(output)
   }
 

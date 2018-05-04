728b71b3a264903c88c7a0bca314ed69cbac4618
zoeang <36051346+zoeang@users.noreply.github.com>
2018-05-04 12:53:19 -0500

    :water_buffalo:

--- devtools.bradleyterry/Rcpp/lambdaLoop.cpp
+++ devtools.bradleyterry/Rcpp/lambdaLoop.cpp
@@ -3,20 +3,24 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-DataFrame lambdaLoop(DataFrame hits, DataFrame lambdas, NumericVector DocIds, DataFrame Hit3){
+DataFrame lambdaLoop(DataFrame hits, DataFrame lambdas, NumericVector DocIds, DataFrame Hit3, DataFrame extractLambdasDF){
   for( int i=0; i<DocIds.size(); ++i){
   int x=DocIds[i];
   Function wich("which");
-  LogicalVector matchedHits=as<int>hits["DocIDi"]== as<int>x;
-  NumericVector hitsIDs= wich(Named("x")=matchedHits);  //'which ' equivalent in rcpp
-  Function con("c");
-  DataFrame newData= Hit3[hitsIDs, con("Choose", "Lambda", "DocIDj")];
+  IntegerVector docHit= hits["DocIDi"];
+  LogicalVector matchedHits= docHit== x;
+  NumericVector hitsIDs= wich(Named("x")=matchedHits);
+  IntegerVector Choose = Hit3["Choose"];
+   NumericVector Lambda = Hit3["Lambda"];
+   IntegerVector DocIDj = Hit3["DocIDj"];
+   DataFrame newData = DataFrame::create(Named("Choose") = Choose[hitsIDs],
+                                         Named("Lambda") = Lambda[hitsIDs],
+                                         Named("DocIDj") = DocIDj[hitsIDs]);
   IntegerVector lambdaDocJ= lambdas["DocIDj"];
-  Function subset("subsetLambdasDF"); //should this be inside the function?
-  DataFrame extractLambda= subset(lambdas, DocIds);
+  //Function subset("subsetLambdasDF"); //should this be inside the function or as argument extractLambdasDF?
+  //DataFrame extractLambda= subset(lambdas, DocIds);
   Function postLamb("posteriorlambda");
-  DataFrame extractlambdas=postLamb(Named("newData")=newData,Named("docXprior")=extractlambdas, Named("a")=1, Named("b")=1);
-  // how to subset DF?
+  DataFrame extractLambdasDF=postLamb(Named("newData")=newData,Named("docXprior")=extractLambdasDF, Named("a")=1, Named("b")=1);
   }
 }
 
--- devtools.bradleyterry/devfile2.R
+++ devtools.bradleyterry/devfile2.R
@@ -111,7 +111,7 @@ HIT3<-merge(hits, lambdas, by="DocIDj") #this is an r function
 # below is a c++ function that calls the c++ function posteriorlambda
 for(i in 1:length(DocIds)){
   x<-DocIds[i]
-  newData<-HIT3[which(Hits$DocIDi==x), c("Choose", "Lambda", "DocIDj")]
+  newData<-HIT3[which(Hits$DocIDi==x), c("Choose", "Lambda", "DocIDj")] #nest an rcpp loop in an r loop?
   lambdas[lambdas$DocIDj==DocIds[i],2]<-posteriorlambda(newData,lambdas[lambdas$DocIDj==DocIds[i],2], a=1, b=1)
 }
 return(lambdas)

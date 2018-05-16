9f37d29a4d51fbaabe9201128dbb85251db55107
zoeang <36051346+zoeang@users.noreply.github.com>
2018-05-16 12:57:19 -0500

    commit

--- devtools.bradleyterry/cleaning_up/R_funs/tolTest.r
+++ devtools.bradleyterry/cleaning_up/R_funs/tolTest.r
@@ -1,11 +1,11 @@
 tolTest<- function(hits, lambdas, DocIds, iterations){
   lambda1<-lambdas$Lambda
-  HIT2$Lambda <- lambda$Lambda[match(HIT2$DocIDj, lambda$DocId)]
+  hits$Lambda <- lambdas$Lambda[match(hits$DocIDj, lambdas$DocId)]
   for( i in 1:iterations){ #change arguments of lambdas
     #HIT3<-merge(hits, lambdas, by="DocIDj") 
-    lambda1<-lambdaLoop2(hits=HIT2, DocIds = DocId,Hit3 = HIT2, extractLambda=lambda1)
+    lambda1<-lambdaLoop2(hits=HIT2, DocIds = DocId, extractLambda=lambda1)
     lambdas$Lambda<-lambda1<-lambda1[-1]
-    HIT2$Lambda <- lambda$Lambda[match(HIT2$DocIDj, lambda$DocId)]
+    hits$Lambda <- lambdas$Lambda[match(hits$DocIDj, lambdas$DocId)]
     if (all(abs(lambda1-lambdas$Lambda)<1e-15)){
       break
     }
